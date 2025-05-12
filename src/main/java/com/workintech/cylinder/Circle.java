3d5cf7cf9b42a52f9e3d3a0fecb4d55fce890e57
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-12 12:37:44 +0300

    circle ok

--- src/main/java/com/workintech/cylinder/Circle.java
+++ src/main/java/com/workintech/cylinder/Circle.java
@@ -1,4 +1,16 @@
 package com.workintech.cylinder;
 
-public class Circle {
+public class Circle {private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius < 0 ? 0 : radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
 }
