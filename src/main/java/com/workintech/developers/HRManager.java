24b23b9fc3d7558d092b8e5c81a2f073f4c8555b
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-12 12:45:13 +0300

    hrmanager ok

--- src/main/java/com/workintech/developers/HRManager.java
+++ src/main/java/com/workintech/developers/HRManager.java
@@ -1,4 +1,50 @@
 package com.workintech.developers;
 
-public class HRManager {
+public class HRManager extends Employee {
+    private JuniorDeveloper[] juniorDevelopers;
+    private MidDeveloper[] midDevelopers;
+    private SeniorDeveloper[] seniorDevelopers;
+
+    public HRManager(int id, String name, double salary) {
+        super(id, name, salary);
+        juniorDevelopers = new JuniorDeveloper[10];
+        midDevelopers = new MidDeveloper[10];
+        seniorDevelopers = new SeniorDeveloper[10];
+    }
+
+    @Override
+    public void work() {
+        System.out.println("HR Manager is managing the hiring process");
+        setSalary(getSalary() + 1000);
+    }
+
+    public void addEmployee(JuniorDeveloper junior) {
+        for (int i = 0; i < juniorDevelopers.length; i++) {
+            if (juniorDevelopers[i] == null) {
+                juniorDevelopers[i] = junior;
+                return;
+            }
+        }
+        System.out.println("No space for new Junior Developer.");
+    }
+
+    public void addEmployee(MidDeveloper mid) {
+        for (int i = 0; i < midDevelopers.length; i++) {
+            if (midDevelopers[i] == null) {
+                midDevelopers[i] = mid;
+                return;
+            }
+        }
+        System.out.println("No space for new Mid Developer.");
+    }
+
+    public void addEmployee(SeniorDeveloper senior) {
+        for (int i = 0; i < seniorDevelopers.length; i++) {
+            if (seniorDevelopers[i] == null) {
+                seniorDevelopers[i] = senior;
+                return;
+            }
+        }
+        System.out.println("No space for new Senior Developer.");
+    }
 }
