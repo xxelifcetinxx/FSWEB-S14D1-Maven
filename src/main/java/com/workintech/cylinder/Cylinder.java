d06a96396e646c20bcbc7911fa22196b910e839f
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-12 12:39:57 +0300

    cylinder ok

--- src/main/java/com/workintech/cylinder/Cylinder.java
+++ src/main/java/com/workintech/cylinder/Cylinder.java
@@ -1,4 +1,18 @@
 package com.workintech.cylinder;
 
-public class Cylinder {
+public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * height;
+    }
 }
